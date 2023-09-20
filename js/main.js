f0a576f803ada93cf9bea1bd2e9223f875f8d0cd
shammasnhd@gmail.com <Shammaz77>
2023-09-20 20:48:33 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function increment() {
+  document.getElementById('demoInput').stepUp();
+}
+function decrement() {
+  document.getElementById('demoInput').stepDown();
+}
+function toggleCartt(){
+  document.querySelector('.sidebar').classList.toggle('open-cart');
+}
